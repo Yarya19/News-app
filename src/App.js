de03ceb221fc09261932f7da8a850111c6edba46
Yash <yasharya9891@gmail.com>
2024-08-27 20:51:41 +0530

    add some components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 import './App.css';
 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoadingBar from 'react-top-loading-bar'
+import LoadingBar from 'react-top-loading-bar';
 
-export default class App extends Component {
-
-  state = {
-    progress:0
-  }
-  updateProgress=(progress)=>{
-    this.setState({
+const App =()=> {
+  const apiKey = process.env.REACT_APP_NEWS_API
+  const [progress,setProgress] = useState(0);
+  const updateProgress=(progress)=>{
+    setProgress({
       progress: progress
     })
   }
 
-  render() {
     return (
       <div>
         <Router>
         <LoadingBar
         color='#f11946'
-        progress={this.state.progress}
+        progress={progress}
       />
         <NavBar/>
         <Routes>
-        <Route exact path="/" element = {<News updateProgress = {this.updateProgress} key="general" pageSize={10} country="in" category="general"/>}></Route>
-        <Route exact path="/business" element = {<News updateProgress = {this.updateProgress} key="business" pageSize={10} country="in" category="business"/>}></Route>
-        <Route exact path="/entertainment"element = {<News updateProgress = {this.updateProgress} key="entertainment" pageSize={10} country="in" category="entertainment"/>}></Route>
-        <Route exact path="/general" element = {<News updateProgress = {this.updateProgress} key="general" pageSize={10} country="in" category="general"/>}></Route>
-        <Route exact path="/health" element = {<News updateProgress = {this.updateProgress} key="health" pageSize={10} country="in" category="health"/>} ></Route>
-        <Route exact path="/science" element = {<News updateProgress = {this.updateProgress} key="science" pageSize={10} country="in" category="science"/>}></Route>
-        <Route exact path="/sports" element = {<News updateProgress = {this.updateProgress} key="sports" pageSize={10} country="in" category="sports"/>}></Route>
-        <Route exact path="/technology" element = {<News updateProgress = {this.updateProgress} key="technology" pageSize={10} country="in" category="technology"/>}></Route>
+        <Route exact path="/" element = {<News updateProgress = {updateProgress} apiKey = {apiKey} key="general" pageSize={10} country="in" category="general"/>}></Route>
+        <Route exact path="/business" element = {<News updateProgress = {updateProgress} apiKey = {apiKey} key="business" pageSize={10} country="in" category="business"/>}></Route>
+        <Route exact path="/entertainment"element = {<News updateProgress = {updateProgress} apiKey = {apiKey} key="entertainment" pageSize={10} country="in" category="entertainment"/>}></Route>
+        <Route exact path="/general" element = {<News updateProgress = {updateProgress} apiKey = {apiKey} key="general" pageSize={10} country="in" category="general"/>}></Route>
+        <Route exact path="/health" element = {<News updateProgress = {updateProgress} apiKey = {apiKey} key="health" pageSize={10} country="in" category="health"/>} ></Route>
+        <Route exact path="/science" element = {<News updateProgress = {updateProgress} apiKey = {apiKey} key="science" pageSize={10} country="in" category="science"/>}></Route>
+        <Route exact path="/sports" element = {<News updateProgress = {updateProgress} apiKey = {apiKey} key="sports" pageSize={10} country="in" category="sports"/>}></Route>
+        <Route exact path="/technology" element = {<News updateProgress = {updateProgress} apiKey = {apiKey} key="technology" pageSize={10} country="in" category="technology"/>}></Route>
         </Routes>
         </Router>
       </div>
     )
-  }
 }
+export default App;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import loading from './loading.gif'
-export default class Spinner extends Component {
-  render() {
+const Spinner=()=> {
     return (
       <div className='text-center my-2'>
         <img src={loading} alt="loading" height={'50px'}/>
       </div>
     )
   }
-}
+export default Spinner
